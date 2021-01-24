3f50083eced9d5e7fd81ef9c0102f8892631d967
Tekiter <tekiter8@gmail.com>
2021-01-24 23:46:56 +0900

    리팩터링: update 함수 내용을 다른 함수로 빼냄

--- visual-ds/renderer/d3/stack.ts
+++ visual-ds/renderer/d3/stack.ts
@@ -1,5 +1,14 @@
+import { Visualizable } from "@/visual-ds/structure/base";
 import { Stack } from "@/visual-ds/structure/stack";
 import * as d3 from "d3";
+
+type Selection<T extends d3.BaseType = undefined, P extends d3.BaseType = null> = d3.Selection<
+    T,
+    undefined,
+    P,
+    undefined
+>;
+type GSelection = Selection<SVGGElement>;
 
 const CELL_SPACE = 5;
 const CELL_WIDTH = 40;
@@ -8,18 +17,24 @@ const FLY_DISTANCE = 40;
 
 export class StackD3Renderer {
     private stack: Stack;
-    private svg: d3.Selection<SVGSVGElement, undefined, null, undefined>;
-    private boxGroup: d3.Selection<SVGGElement, undefined, SVGSVGElement, undefined>;
-    private textGroup: d3.Selection<SVGGElement, undefined, SVGSVGElement, undefined>;
+    private svg: Selection<SVGSVGElement>;
+    private boxGroup: GSelection;
+    private textGroup: GSelection;
 
     constructor(stack: Stack) {
         this.stack = stack;
         this.stack.addActionHandler("push", this.onPush.bind(this));
         this.stack.addActionHandler("pop", this.onPop.bind(this));
 
-        this.svg = d3.create("svg").attr("viewBox", "0 0 500 500");
-        this.boxGroup = this.svg.append("g");
-        this.textGroup = this.svg.append("g").attr("fill", "black").attr("text-anchor", "middle");
+        this.svg = d3.create("svg").attr("viewBox", "0 0 800 800");
+        this.boxGroup = this.svg.append("g").attr("class", "box");
+        this.textGroup = this.svg
+            .append("g")
+            .attr("class", "text")
+            .attr("fill", "black")
+            .attr("text-anchor", "middle");
+
+        this.svg.append("g").attr("class", "index").attr("text-anchor", "middle");
 
         this.update();
     }
@@ -33,85 +48,117 @@ export class StackD3Renderer {
     }
 
     update(): void {
-        const trans = d3.transition().duration(750).ease(d3.easeCubicOut);
-
-        this.boxGroup
-            .selectAll("rect")
-            .data(this.stack.expose.stack)
-            .join(
-                (enter) =>
-                    enter
-                        .append("rect")
-                        .attr("x", (_, i: number) => (CELL_WIDTH + CELL_SPACE) * i + FLY_DISTANCE)
-                        .attr("opacity", 0.0)
-                        .call((enter) =>
-                            enter
-                                .transition(trans)
-                                .duration(750)
-                                .attr("x", (_, i: number) => (CELL_WIDTH + CELL_SPACE) * i)
-                                .attr("opacity", 1.0)
-                        ),
-                (update) => update,
-                (exit) =>
-                    exit.call((exit) =>
-                        exit
-                            .transition(trans)
-                            .attr("opacity", 0.0)
-                            .attr(
-                                "x",
-                                (_, i: number) => (CELL_WIDTH + CELL_SPACE) * i + FLY_DISTANCE
-                            )
-                            .remove()
-                    )
-            )
-
-            .attr("y", 30)
-            .attr("height", CELL_HEIGHT)
-            .attr("width", CELL_WIDTH)
-            .attr("fill", "white")
-            .attr("stroke", "black")
-            .attr("stroke-width", 2);
-
-        this.textGroup
-            .selectAll("text")
-            .data(this.stack.expose.stack)
-            .join(
-                (enter) =>
-                    enter
-                        .append("text")
-                        .attr(
-                            "x",
-                            (_, i: number) =>
-                                (CELL_WIDTH + CELL_SPACE) * i + CELL_WIDTH / 2 + FLY_DISTANCE
-                        )
-                        .attr("fill-opacity", 0.0)
-                        .call((enter) =>
-                            enter
-                                .transition(trans)
-                                .duration(750)
-                                .attr("x", (_, i: number) => (CELL_WIDTH + CELL_SPACE) * i + 20)
-                                .attr("fill-opacity", 1.0)
-                        ),
-                (update) => update,
-                (exit) =>
-                    exit.call((exit) =>
-                        exit
-                            .transition(trans)
-                            .attr("fill-opacity", 0.0)
-                            .attr(
-                                "x",
-                                (_, i: number) =>
-                                    (CELL_WIDTH + CELL_SPACE) * i + CELL_WIDTH / 2 + 40
-                            )
-                            .remove()
-                    )
-            )
-            .text((d) => d.toString())
-            .attr("y", CELL_HEIGHT + CELL_HEIGHT / 2)
-            .attr("dy", "0.35em");
+        drawBox(this.boxGroup, this.stack.expose.stack);
+        drawText(this.textGroup, this.stack.expose.stack);
+        drawIndex(this.svg.select("g.index"), this.stack.expose.stack);
     }
 
     node(): SVGSVGElement {
         return this.svg.node();
     }
+}
+
+function drawBox(group: GSelection, stack: Visualizable[]) {
+    const trans = d3.transition().duration(750).ease(d3.easeCubicOut);
+
+    return group
+        .selectAll("rect")
+        .data(stack)
+        .join(
+            (enter) =>
+                enter
+                    .append("rect")
+                    .attr("x", (_, i: number) => (CELL_WIDTH + CELL_SPACE) * i + FLY_DISTANCE)
+                    .attr("opacity", 0.0)
+                    .call((enter) =>
+                        enter
+                            .transition(trans)
+                            .attr("x", (_, i: number) => (CELL_WIDTH + CELL_SPACE) * i)
+                            .attr("opacity", 1.0)
+                    ),
+            (update) => update,
+            (exit) =>
+                exit.call((exit) =>
+                    exit
+                        .transition(trans)
+                        .attr("opacity", 0.0)
+                        .attr("x", (_, i: number) => (CELL_WIDTH + CELL_SPACE) * i + FLY_DISTANCE)
+                        .remove()
+                )
+        )
+
+        .attr("y", 30)
+        .attr("height", CELL_HEIGHT)
+        .attr("width", CELL_WIDTH)
+        .attr("fill", "white")
+        .attr("stroke", "black")
+        .attr("stroke-width", 2);
+}
+
+function drawText(group: GSelection, stack: Visualizable[]) {
+    const trans = d3.transition().duration(750).ease(d3.easeCubicOut);
+
+    return group
+        .selectAll("text.val")
+        .data(stack)
+        .join(
+            (enter) =>
+                enter
+                    .append("text")
+                    .attr("class", "val")
+                    .attr(
+                        "x",
+                        (_, i: number) =>
+                            (CELL_WIDTH + CELL_SPACE) * i + CELL_WIDTH / 2 + FLY_DISTANCE
+                    )
+                    .attr("fill-opacity", 0.0)
+                    .call((enter) =>
+                        enter
+                            .transition(trans)
+                            .attr("x", (_, i: number) => (CELL_WIDTH + CELL_SPACE) * i + 20)
+                            .attr("fill-opacity", 1.0)
+                    ),
+            (update) => update,
+            (exit) =>
+                exit.call((exit) =>
+                    exit
+                        .transition(trans)
+                        .attr("fill-opacity", 0.0)
+                        .attr(
+                            "x",
+                            (_, i: number) => (CELL_WIDTH + CELL_SPACE) * i + CELL_WIDTH / 2 + 40
+                        )
+                        .remove()
+                )
+        )
+        .text((d) => d.toString())
+        .attr("y", CELL_HEIGHT + CELL_HEIGHT / 2)
+        .attr("dy", "0.35em");
+}
+
+function drawIndex(group: GSelection, stack: Visualizable[]) {
+    const trans = d3.transition().duration(750).ease(d3.easeCubicOut);
+
+    return group
+        .selectAll("text.idx")
+        .data(stack)
+        .join(
+            (enter) =>
+                enter
+                    .append("text")
+                    .attr("class", "idx")
+                    .attr("x", (_, i: number) => (CELL_WIDTH + CELL_SPACE) * i + 20)
+                    .attr("fill-opacity", 0.0)
+                    .attr("y", CELL_HEIGHT - 10 - 30)
+                    .call((enter) =>
+                        enter
+                            .transition(trans)
+                            .attr("fill-opacity", 1.0)
+                            .attr("y", CELL_HEIGHT - 10)
+                    ),
+            (update) => update,
+            (exit) => exit.transition(trans).attr("fill-opacity", 0.0).remove()
+        )
+        .text((_, i: number) => i)
+        .attr("dy", "0.35em");
 }
