ce8a2664a1187523e91c43d9078a2de569ef44db
goseungduk <a42873410@gmail.com>
2021-01-25 15:42:50 +0900

    test

--- visual-ds/structure/stack.ts
+++ visual-ds/structure/stack.ts
@@ -3,35 +3,36 @@ import { DSBase, Visualizable, DSObservable } from "./base";
 export interface StackExpose<T extends Visualizable = Visualizable> {
     stack: T[];
 }
+// T extends Visualizable=Visualizable equals to TExpose type.
 
 export class Stack<T extends Visualizable = Visualizable>
     extends DSObservable<StackExpose<T>>
     implements DSBase {
-    public readonly name = "Stack";
+        public readonly name = "Stack";
 
-    private stk: T[];
+        private stk: T[];
 
-    constructor() {
-        super();
-        this.stk = [];
+        constructor() {
+            super();
+            this.stk = [];
 
-        this.onExpose(() => ({
-            stack: [...this.stk]
-        }));
-    }
+            this.onExpose(() => ({
+                stack: [...this.stk]
+            }));
+        }
 
-    push(value: T): void {
-        this.stk.push(value);
-        this.notifyChange("push", { value });
-    }
+        push(value: T): void {
+            this.stk.push(value);
+            this.notifyChange("push", { value });
+        }
 
-    pop(): T {
-        const value = this.stk.pop();
-        this.notifyChange("pop", { value });
-        return value;
-    }
+        pop(): T {
+            const value = this.stk.pop();
+            this.notifyChange("pop", { value });
+            return value;
+        }
 
-    size(): number {
-        return this.stk.length;
-    }
+        size(): number {
+            return this.stk.length;
+        }
 }
--- visual-ds/structure/test.ts
+++ visual-ds/structure/test.ts
@@ -1,1 +1,6 @@
-asdf
+function first<T>(arr: T[]): T {
+    console.log(typeof(arr[0]));
+    return arr[0];
+}
+console.log(first<number>([1,2,3]));
+console.log(first<string>(['1','2','3']));
