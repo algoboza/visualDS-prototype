e9e28a9989643a7411da59e956222174653ef711
Tekiter <tekiter8@gmail.com>
2021-01-25 16:47:45 +0900

    추가: 디자인, 레이블, TOP 포인터

--- visual-ds/renderer/d3/stack.ts
+++ visual-ds/renderer/d3/stack.ts
@@ -17,24 +17,28 @@ const FLY_DISTANCE = 40;
 
 export class StackD3Renderer {
     private stack: Stack;
+    private root: GSelection;
     private svg: Selection<SVGSVGElement>;
     private boxGroup: GSelection;
     private textGroup: GSelection;
+    private labelGroup: GSelection;
 
     constructor(stack: Stack) {
         this.stack = stack;
         this.stack.addActionHandler("push", this.onPush.bind(this));
         this.stack.addActionHandler("pop", this.onPop.bind(this));
 
-        this.svg = d3.create("svg").attr("viewBox", "0 0 800 800");
-        this.boxGroup = this.svg.append("g").attr("class", "box");
-        this.textGroup = this.svg
-            .append("g")
-            .attr("class", "text")
-            .attr("fill", "black")
-            .attr("text-anchor", "middle");
+        this.svg = d3.create("svg").attr("viewBox", "0 0 800 200");
 
-        this.svg.append("g").attr("class", "index").attr("text-anchor", "middle");
+        this.root = this.svg.append("g").attr("transform", "translate(50, 0)");
+
+        this.boxGroup = this.root.append("g").attr("class", "box");
+        this.textGroup = this.root.append("g").attr("class", "text");
+        this.labelGroup = this.root.append("g");
+
+        drawLabel(this.root.append("g"));
+
+        this.root.append("g").attr("class", "index");
 
         this.update();
     }
@@ -48,9 +52,12 @@ export class StackD3Renderer {
     }
 
     update(): void {
-        drawBox(this.boxGroup, this.stack.expose.stack);
-        drawText(this.textGroup, this.stack.expose.stack);
-        drawIndex(this.svg.select("g.index"), this.stack.expose.stack);
+        const stk = this.stack.expose.stack;
+
+        drawBox(this.boxGroup, stk);
+        drawText(this.textGroup, stk);
+        drawIndex(this.root.select("g.index"), stk);
+        drawPointer(this.labelGroup, stk);
     }
 
     node(): SVGSVGElement {
@@ -60,6 +67,8 @@ export class StackD3Renderer {
 
 function drawBox(group: GSelection, stack: Visualizable[]) {
     const trans = d3.transition().duration(750).ease(d3.easeCubicOut);
+
+    group.attr("fill", "#660eb3");
 
     return group
         .selectAll("rect")
@@ -89,14 +98,13 @@ function drawBox(group: GSelection, stack: Visualizable[]) {
 
         .attr("y", 30)
         .attr("height", CELL_HEIGHT)
-        .attr("width", CELL_WIDTH)
-        .attr("fill", "white")
-        .attr("stroke", "black")
-        .attr("stroke-width", 2);
+        .attr("width", CELL_WIDTH);
 }
 
 function drawText(group: GSelection, stack: Visualizable[]) {
     const trans = d3.transition().duration(750).ease(d3.easeCubicOut);
+
+    group.attr("fill", "white").attr("text-anchor", "middle");
 
     return group
         .selectAll("text.val")
@@ -139,6 +147,8 @@ function drawText(group: GSelection, stack: Visualizable[]) {
 function drawIndex(group: GSelection, stack: Visualizable[]) {
     const trans = d3.transition().duration(750).ease(d3.easeCubicOut);
 
+    group.attr("text-anchor", "middle").attr("font-size", 13).attr("fill", "grey");
+
     return group
         .selectAll("text.idx")
         .data(stack)
@@ -161,4 +171,75 @@ function drawIndex(group: GSelection, stack: Visualizable[]) {
         )
         .text((_, i: number) => i)
         .attr("dy", "0.35em");
+}
+
+function drawLabel(group: GSelection) {
+    group
+        .append("text")
+        .text("Index")
+        .attr("font-size", 10)
+        .attr("x", -10)
+        .attr("y", CELL_HEIGHT - 5)
+        .attr("text-anchor", "end");
+
+    group
+        .append("text")
+        .text("Data")
+        .attr("font-size", 10)
+        .attr("x", -10)
+        .attr("y", CELL_HEIGHT * 2 - 5)
+        .attr("text-anchor", "end");
+}
+
+function drawPointer(group: GSelection, stack: Visualizable[]) {
+    const trans = d3.transition().duration(750).ease(d3.easeCubicOut);
+
+    // group.html(
+    //     `<path fill="currentColor" d="M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z" />`
+    // );
+
+    group
+        .selectAll("path.topptr")
+        .data([stack.length])
+        .join(
+            (enter) =>
+                enter
+                    .append("path")
+                    .attr("class", "topptr")
+                    .attr("d", "M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z")
+                    .attr("transform", `translate(${-CELL_SPACE - 12}, ${CELL_HEIGHT * 2})`),
+            (update) =>
+                update
+                    .call((update) =>
+                        update
+                            .transition(trans)
+                            .attr(
+                                "transform",
+                                (d) =>
+                                    `translate(${
+                                        (CELL_WIDTH + CELL_SPACE) * d - CELL_SPACE - 12
+                                    }, ${CELL_HEIGHT * 2})`
+                            )
+                    )
+                    .attr("fill-color", "red"),
+            (exit) => exit
+        );
+
+    group
+        .selectAll("text.topptr")
+        .data([stack.length])
+        .join(
+            (enter) => enter.append("text").attr("class", "topptr").attr("x", -CELL_SPACE),
+            (update) =>
+                update.call((update) =>
+                    update
+                        .transition(trans)
+                        .attr("x", (d) => (CELL_WIDTH + CELL_SPACE) * d - CELL_SPACE)
+                ),
+            (exit) => exit
+        )
+        .text("TOP")
+        .attr("text-anchor", "middle")
+        .attr("dy", "0.75em")
+        .attr("y", CELL_HEIGHT * (2 + 2 / 3));
 }
