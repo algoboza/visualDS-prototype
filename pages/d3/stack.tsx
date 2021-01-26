427c72d2c12a4348088b38b4db4df4e8af4ebd21
Tekiter <tekiter8@gmail.com>
2021-01-26 20:20:47 +0900

    추가: 간단한 Serializer

--- pages/d3/stack.tsx
+++ pages/d3/stack.tsx
@@ -2,12 +2,13 @@ import { memo, MutableRefObject, useEffect, useRef, useState, VFC } from "react"
 import { StackController } from "@/components/controllers/stack";
 import { Stack } from "@/visual-ds/structure/stack";
 import { StackD3Renderer } from "@/visual-ds/renderer/d3/stack";
+import { Visualizable } from "@/visual-ds/structure/base";
 
-interface VisualizerProps {
+interface StackVisualizerProps {
     stackRef: MutableRefObject<Stack>;
 }
 
-const Visualizer = memo<VisualizerProps>(
+const StackVisualizer = memo<StackVisualizerProps>(
     function Visualizer({ stackRef }) {
         const renderer = useRef<StackD3Renderer>(null);
         const container = useRef<HTMLDivElement>(null);
@@ -26,6 +27,27 @@ const Visualizer = memo<VisualizerProps>(
         return false;
     }
 );
+
+const StackSerializer = (props: { data: Visualizable[] }) => {
+    const { data = [] } = props;
+    function serialize(data: Visualizable[]) {
+        let ret = "";
+
+        ret += data.length + "\n";
+        for (let v of data) {
+            ret += `${v} `;
+        }
+        ret += "\n";
+
+        return ret;
+    }
+
+    return (
+        <div>
+            <pre>{serialize(data)}</pre>
+        </div>
+    );
+};
 
 export default (function StackD3() {
     const stack = useRef<Stack>(new Stack());
@@ -47,8 +69,8 @@ export default (function StackD3() {
     return (
         <div>
             <StackController onPush={handlePush} onPop={handlePop} />
-            <Visualizer stackRef={stack} />
-            {JSON.stringify(currentStack)}
+            <StackVisualizer stackRef={stack} />
+            <StackSerializer data={currentStack} />
         </div>
     );
 } as VFC);
